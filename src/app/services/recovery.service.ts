7a55a9c272cb2d7237c0c8fab5d679b380da68da
Matias Alejo Garcia <ematiu@gmail.com>
2018-02-12 14:22:48 -0300

    add ALL copayer paths

--- src/app/services/recovery.service.ts
+++ src/app/services/recovery.service.ts
@@ -30,7 +30,7 @@ export class RecoveryService {
   constructor(private http: HttpClient) {
     this.PATHS = {
       // we found some broken BIP45 wallet, that have some BIP44 addresses, so:
-      'BIP45': ["m/45'/2147483647/0", "m/45'/2147483647/1", , "m/45'/0/1", , "m/45'/0/1"],
+      'BIP45': ["m/45'/2147483647/0", "m/45'/2147483647/1" ],
       'BIP44': {
         'testnet': ["m/44'/1'/0'/0", "m/44'/1'/0'/1"],
         'livenet': ["m/44'/0'/0'/0", "m/44'/0'/0'/1"],
@@ -62,7 +62,8 @@ export class RecoveryService {
     }
     if ((payload.m != m) || (payload.n != n)) {
       throw new Error("The wallet configuration (m-n) does not match with values provided.");
-    }
+      }
+
     if (payload.network != network) {
       throw new Error("Incorrect network.");
     }
@@ -169,6 +170,7 @@ export class RecoveryService {
   getWallet(data: any, m: number, n: number, coin: string, network: string) {
     var self = this;
     var credentials = _.map(data, (dataItem: any) => {
+      dataItem.backup = _.trim(dataItem.backup);
       if (dataItem.backup.charAt(0) == '{')
         return this.fromBackup(dataItem, m, n, coin, network);
       else
@@ -204,8 +206,15 @@ export class RecoveryService {
   }
 
   getPaths(wallet: any) {
-    if (wallet.derivationStrategy == 'BIP45')
-      return this.PATHS[wallet.derivationStrategy];
+    if (wallet.derivationStrategy == 'BIP45') {
+      var p = _.clone(this.PATHS[wallet.derivationStrategy]);
+      // adds copayer's paths
+      for(let i=0; i<wallet.n; i++) {
+        var copayerPaths = ["m/45'/"+i+"/0",  "m/45'/"+i+"/1"];
+        p = p.concat(copayerPaths);
+      };
+      return p;
+    }
     if (wallet.derivationStrategy == 'BIP44')
       return this.PATHS[wallet.derivationStrategy][wallet.network];
   };
